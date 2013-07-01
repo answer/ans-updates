323f707ae4566d66a5cf3c9c85585fde2ec382d1
sakai shunsuke <sakai@ans-web.co.jp>
2013-07-01 19:12:28 +0900

    fix spec helper

--- lib/ans-updates/spec_helper.rb
+++ lib/ans-updates/spec_helper.rb
@@ -4,14 +4,14 @@ module Ans::Updates
   module SpecHelper
 
     def self.for_view_spec
+      Helper.send :include, ViewSpecHelper
+    end
 
-      module Helper
-        # view のテストでルーティングエラーになる問題を回避
-        def updates_get_link(label,opts={})
-          nil
-        end
+    module ViewSpecHelper
+      # view のテストでルーティングエラーになる問題を回避
+      def updates_get_link(label,opts={})
+        nil
       end
-
     end
 
   end
